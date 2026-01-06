7ec57378f65499152c219132a39b359136b94022
r06070411-dev <r06070411@gmail.com>
2026-01-06 19:28:00 +0900

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("enterBtn").addEventListener("click", () => {
-  document.getElementById("result").style.display = "block";
-  document.getElementById("currentArea").textContent = "Fashion District";
-});
-
-document.getElementById("backBtn").addEventListener("click", () => {
-  document.getElementById("result").style.display = "none";
-});
